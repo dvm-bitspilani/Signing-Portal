fa179c9551b1a70916e31938602a3b09b96ce5b9
sumit9722 <shahsumit9722@gmail.com>
2025-05-31 00:23:03 +0530

    Google Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import SignIn from "./pages/SignIn/SignIn.jsx";
 import styles from "./App.module.scss";
-import { SignInProvider } from "./assets/store/SignInContext.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
+import { useContext } from "react";
+import SignInContext from "./assets/store/SignInContext.jsx";
 
 const App = () => {
+  const { isSignIn } = useContext(SignInContext);
   return (
     <BrowserRouter>
-      <SignInProvider>
-        <div className={styles.app}>
-          <Routes>
-            <Route path="/signin" element={<SignIn />} />
-            <Route index element={<Home />} />
-            //? Add Routes linked to other pages here
-
-          </Routes>
-        </div>
-      </SignInProvider>
+      <div className={styles.app}>
+        <Routes>
+          <Route path="/" element={isSignIn ? <Home /> : <SignIn />} />
+          //? Add Routes linked to other pages here
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
