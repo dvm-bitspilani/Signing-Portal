efce4579f811b33c3e7913f9194b9b6612666cc1
varprabhlite <185780179+varprabhlite@users.noreply.github.com>
2025-06-04 14:40:33 +0530

    resolve conflict in src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import React from "react";
-import SignIn from "./pages/SignIn/SignIn.jsx";
-import styles from "./App.module.scss";
+import { useState, useContext, createContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home.jsx";
-import { useContext } from "react";
+
+import styles from "./App.module.scss";
+
 import SignInContext from "./assets/store/SignInContext.jsx";
-import EventDetails from "./pages/EventDetails/EventDetails.jsx";
+
+import SignIn from "./pages/SignIn/SignIn.jsx";
+import Home from "./pages/Home/Home.jsx";
+import YourSignings from "./pages/YourSignings/YourSignings.jsx";
+
+export const AppContext = createContext({});
 
 const App = () => {
   const { isSignIn } = useContext(SignInContext);
+  //* Use this for all other uses of contexts
+  const [globalAppStates, setGlobalAppStates] = useState({});
+
   return (
     <BrowserRouter>
-      <div className={styles.app}>
-        <Routes>
-          <Route path = "/" element={isSignIn ? <Home /> : <SignIn />} />
-          <Route path = "/EventDetails/:eventIndex" element={<EventDetails />}/> 
-          //? Add Routes linked to other pages here
-        </Routes>
-      </div>
+      <AppContext.Provider value={{ globalAppStates, setGlobalAppStates }}>
+        <div className={styles.app}>
+          <Routes>
+            <Route path="/" element={isSignIn ? <Home /> : <SignIn />} />
+            <Route
+              path="/yoursignings"
+              element={isSignIn ? <YourSignings /> : <SignIn />}
+            />
+            <Route path = "/EventDetails/:eventIndex" element={<EventDetails />}/> 
+            {/* Add Routes linked to other pages here */}
+          </Routes>
+        </div>
+      </AppContext.Provider>
     </BrowserRouter>
   );
 };
