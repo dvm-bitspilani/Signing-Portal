a9dca5fcfef10a550a6e3390591aa8ff21673e01
rish-kun <63300310+rish-kun@users.noreply.github.com>
2026-02-05 20:35:21 +0530

    back

--- src/global.js
+++ src/global.js
@@ -1,16 +1,17 @@
 import React from "react";
 
-export const apiBaseURL = "https://www.bits-apogee.org/2026/main/signings";
-export const merchBaseURL = "https://www.bits-apogee.org/2026/main/tickets-manager";
+export const apiBaseURL = "https://www.bits-oasis.org/2025/main/signings";
+export const merchBaseURL =
+  "https://www.bits-oasis.org/2025/main/tickets-manager";
 //* Adds/sets a key-value pair to an 'object' state
 export function setStateItem(state, key, value) {
-    const object = {...state};
-    object[key] = value;
-    return object;
+  const object = { ...state };
+  object[key] = value;
+  return object;
 }
 
 export function setStateItems(state, obj) {
-    const tempObject = {...state};
-    for (let key in obj) tempObject[key] = obj[key];
-    return tempObject;
+  const tempObject = { ...state };
+  for (let key in obj) tempObject[key] = obj[key];
+  return tempObject;
 }
