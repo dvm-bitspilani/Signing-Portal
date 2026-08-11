313f51716df9cce97d49c8eb923654db7be7a7a9
Abhiraj <abhirajpharate@gmail.com>
2026-08-11 22:14:48 +0530

    oasis logo and google client change

--- keyconfig.js
+++ keyconfig.js
@@ -1,3 +1,3 @@
-export const GOOGLE_CLIENT_ID = "650062736141-pb64o0ubh8ejek13gogejjb9t5r6sdf0.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_ID = "609870011994-vbtfuu5ql7t397k9etp1n1ohgdskvqhg.apps.googleusercontent.com";
 export const API_BASE_URL = "https://www.bits-oasis.org/2026/main/signings";
 export const MERCH_BASE_URL = "https://www.bits-oasis.org/2026/main/tickets-manager";
